e70da9ad7caa99dc10b61823eb2a27f338cd08ae
123Nishiwei <shiwei.ni>
2022-06-22 14:04:50 +0800

    n

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,9 +3,14 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    const { ctx } = this;
-    ctx.body = 'hi, egg';
+  async list() {
+    const dataList = {
+      list: [
+        { id: 1, title: 'this is news 1', url: '/news/1' },
+        { id: 2, title: 'this is news 2', url: '/news/2' },
+      ],
+    };
+    this.ctx.body = dataList;
   }
 }
 
